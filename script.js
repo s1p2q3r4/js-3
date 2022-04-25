d078f1e9c4a0fb4765231dec5dff46108bf2aafc
s1p2q3r4 <99183896+s1p2q3r4@users.noreply.github.com>
2022-04-25 17:36:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+function seat ()
+ 
+     if (Seat number==2,5,10,13,18,21,26,29,34,7,42,45,40,53,58,61,66,69){
+        console.log("Middle Berth")
+    } else if (Seat number==3,6,11,14,19,22,27,30,35,38,43,46,51,54,59,62,67,70){
+        console.log("Upper Berth")
+    }else if(Seat number==7,5,23,31,39,47,55,63,71){
+        console.log("Side Lower Berth")
+    }else if(Seat number==8,16,24,32,40,48,56,64,72){
+        console.log("Side Upper Berth")
+    }
+    else{
+        console.log("NO Seat reserved in  this number")
+    }
+
+    
+
+
+ 
